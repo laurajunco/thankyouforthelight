2b39da8f1ddefc53e5fd3ebfe59f77259d0278a9
Laura Junco <l.juncogomez@celonis.com>
2025-06-26 20:42:33 +0200

    try blog template

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Thank you for the light",
+	url: "https://thankyouforthelight.xyz/",
+	language: "en",
+	description: "I am writing about my experiences",
+	author: {
+		name: "Laura",
+		email: "laura@example.com",
+		url: "https://thankyouforthelight.xyz/"
+	}
+}
